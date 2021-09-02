fcc96f9af03b48f43019969da3369b28c2ffffd5
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-09-03 00:59:32 +0200

    starter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three'
+import React , {useEffect, useState}from 'react';
+import useWindowDimensions from './WidthHook'
+
 
 function App() {
+  const { width , height } = useWindowDimensions();
+  const scene = new THREE.Scene();
+//red cube
+const group = new THREE.Group();
+scene.add(group);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+group.position.set (0,.5,0) 
+group.scale.set (2,3,2) 
+group.rotation.y = 1;
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, width / height);
+camera.position.z = 6.5;
+scene.add(camera);
+
+  useEffect(() => {
+    
+    const canvas = document.querySelector(".webgl");
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+    });
+    renderer.setSize(width, height);
+    
+    renderer.render(scene, camera);
+  }, [width , height]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <canvas class="webgl" ></canvas>
+   </>
   );
 }
 
